49d0628082a376f93645f012cb4357d8efb450d3
Vinoth Kannan <vinothkannan@vinkas.com>
2016-09-05 22:39:21 +0530

    NEW: label & button language files

---
+++ resources/lang/en/button.php
@@ -0,0 +1,16 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Form Buttons Language Lines
+    |--------------------------------------------------------------------------
+    |
+    | The following language lines are used on form labels
+    |
+    */
+
+    'register' => 'Create account',
+
+];
