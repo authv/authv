bef8801b264782ada37614172e0bc4d0c6bb1d96
Vinoth Kannan <vinothkannan@vinkas.com>
2016-09-06 18:14:06 +0530

    NEW: db migrate for email tokens

---
+++ database/migrations/2016_09_06_120000_create_email_tokens_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateEmailTokensTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('email_tokens', function (Blueprint $table) {
+            $table->integer('user_id')->unsigned();
+            $table->increments('id');
+            $table->string('email');
+            $table->string('token')->unique();
+            $table->boolean('confirmed')->default(false);
+            $table->boolean('expired')->default(false);
+            $table->timestamps();
+            $table->foreign('user_id')->references('id')->on('users');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('email_tokens');
+    }
+}
